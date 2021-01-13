51387c5bc8a3cdcd083804a09ad45f3f29338c8a
Alessandro Schneider <alessandro.schneider@campus.tu-berlin.de>
2021-01-13 18:25:46 +0100

    feat: app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  questions: any[] = [
+    {
+      "id": 1,
+      "title": "Do you find the app easy to use?",
+      "type": "checkbox"
+
+    },
+    {
+      "id": 2,
+      "title": "Do you understand how AI is integrated in the application?",
+      "type": "checkbox"
+    }, {
+      "id": 3,
+      "title": "Would you use the application often?",
+      "type": "checkbox"
+
+    }, {
+      "id": 4,
+      "title": "What was the best/worst thing about the app?",
+      "type": "text"
+
+    }, {
+      "id": 5,
+      "title": "Do you have any other feedback?",
+      "type": "text"
+
+    },
+  ]
+
+
   title = 'usertesting';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {QuestionComponent} from './question/question.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatRadioModule} from "@angular/material/radio";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent
   ],
   imports: [
-    BrowserModule
+    MatRadioModule,
+    MatInputModule,
+    MatFormFieldModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
